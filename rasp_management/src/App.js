43d70fd0d696397d63a7b5144806bc0638e73891
AnDinhquangC0 <20520370@gm.uit.edu.vn>
2024-10-06 17:13:59 +0700

    card update
    add card and update node navbar

--- rasp_management/src/App.js
+++ rasp_management/src/App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css'; 
 import Navbar from './navbar/navbar'; 
+import Nodes from './nodes/Nodes'; // Đường dẫn đến component Nodes
+// import Overview from './overview/Overview'; // Nếu bạn có một component Overview
+// import Services from './services/Services'; // Nếu bạn có một component Services
+// import Logs from './logs/Logs'; // Nếu bạn có một component Logs
 
 const Overview = () => <h1>Overview Page</h1>;
-const Nodes = () => <h1>Nodes Page</h1>;
+// const Nodes = () => <h1>Nodes Page</h1>;
 const Services = () => <h1>Services Page</h1>;
 const Logs = () => <h1>Logs Page</h1>;
 
@@ -27,6 +31,8 @@ function App() {
                 </div>
             </div>
         </Router>
+
+    
     );
 }
 
