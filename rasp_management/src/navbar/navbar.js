cc162ab1644032f470c22c6cfa85dc2a9afd5b58
AnDinhquangC0 <20520370@gm.uit.edu.vn>
2024-10-09 10:48:00 +0700

    test finish

--- rasp_management/src/navbar/navbar.js
+++ rasp_management/src/navbar/navbar.js
@@ -1,3 +1,27 @@
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import './navbar.css';
+
+// const Navbar = () => {
+//     return (
+//         <div className="navbar">
+//             <NavLink to="/overview" className="nav-link" activeClassName="active">
+//                 Overview
+//             </NavLink>
+//             <NavLink to="/nodes" className="nav-link" activeClassName="active">
+//                 Nodes
+//             </NavLink>
+//             <NavLink to="/services" className="nav-link" activeClassName="active">
+//                 Services
+//             </NavLink>
+//             <NavLink to="/logs" className="nav-link" activeClassName="active">
+//                 Logs
+//             </NavLink>
+//         </div>
+//     );
+// };
+
+// export default Navbar;
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
@@ -5,16 +29,24 @@ import './navbar.css';
 const Navbar = () => {
     return (
         <div className="navbar">
-            <NavLink to="/overview" className="nav-link" activeClassName="active">
+            <NavLink 
+                to="/overview" 
+                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
                 Overview
             </NavLink>
-            <NavLink to="/nodes" className="nav-link" activeClassName="active">
+            <NavLink 
+                to="/nodes" 
+                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
                 Nodes
             </NavLink>
-            <NavLink to="/services" className="nav-link" activeClassName="active">
+            <NavLink 
+                to="/services" 
+                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
                 Services
             </NavLink>
-            <NavLink to="/logs" className="nav-link" activeClassName="active">
+            <NavLink 
+                to="/logs" 
+                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
                 Logs
             </NavLink>
         </div>
