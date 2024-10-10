ce43493c31d2a0e48f239e66d5b41f21c4af5a03
AnhTr14 <20520137@gm.uit.edu.vn>
2024-10-10 12:51:10 +0700

    add button scan to open modal

--- rasp_management/src/nodes/Nodes.jsx
+++ rasp_management/src/nodes/Nodes.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import { Container, Grid2 } from '@mui/material';
 import data from '../data.json';
 import NodeCard from '../nodes/Card'; // Import the Card component
+import ModalNodesList from '../components/modal_nodes_list'; // Import the ModalNodesList component
+import { useState } from 'react';
 
 export default function Nodes() {
+    const [showModal, setShowModal] = useState(false);
+    const handleShow = () => setShowModal(true);
+    const handleClose = () => setShowModal(false);
+
   return (
     <div>
         <div>
             <h1>Nodes Page</h1>
         </div>
+        <div className='d-flex justify-content-end pb-3'>
+            <button className='btn btn-primary me-1' onClick={handleShow}><i className="fa-brands fa-cloudscale"></i> Scan</button>
+            <button className='btn btn-primary'><i className="fas fa-download"></i> Update Nodes</button>
+        </div>
+        <ModalNodesList isShow={showModal} handleClose={handleClose}/>
         <div div className='cardlist overflow-y-scroll max-h-96 pr-2' style={{ overflowY: 'auto', maxHeight: '30rem'}}>
             <Container maxWidth='xl'>
                 <Grid2 container spacing={3} style={{ marginTop: "10px" }}>
