61478bd899ffff5a257616c4cbab49b15a7390d0
AnDinhquangC0 <20520370@gm.uit.edu.vn>
2024-10-10 20:45:14 +0700

    test xong

--- rasp_management/src/components/search_bar.js
+++ rasp_management/src/components/search_bar.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 const SearchBar = ({ onSearch }) => {
--- rasp_management/src/navbar/navbar.js
+++ rasp_management/src/navbar/navbar.js
@@ -29,24 +29,16 @@ import './navbar.css';
 const Navbar = () => {
     return (
         <div className="navbar">
-            <NavLink 
-                to="/overview" 
-                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+            <NavLink to="/overview" className="nav-link" activeClassName="active">
                 Overview
             </NavLink>
-            <NavLink 
-                to="/nodes" 
-                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+            <NavLink to="/nodes" className="nav-link" activeClassName="active">
                 Nodes
             </NavLink>
-            <NavLink 
-                to="/services" 
-                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+            <NavLink to="/services" className="nav-link" activeClassName="active">
                 Services
             </NavLink>
-            <NavLink 
-                to="/logs" 
-                className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+            <NavLink to="/logs" className="nav-link" activeClassName="active">
                 Logs
             </NavLink>
         </div>
@@ -54,3 +46,35 @@ const Navbar = () => {
 };
 
 export default Navbar;
+// import React from 'react';
+// import { NavLink } from 'react-router-dom';
+// import './navbar.css';
+
+// const Navbar = () => {
+//     return (
+//         <div className="navbar">
+//             <NavLink 
+//                 to="/overview" 
+//                 className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+//                 Overview
+//             </NavLink>
+//             <NavLink 
+//                 to="/nodes" 
+//                 className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+//                 Nodes
+//             </NavLink>
+//             <NavLink 
+//                 to="/services" 
+//                 className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+//                 Services
+//             </NavLink>
+//             <NavLink 
+//                 to="/logs" 
+//                 className={({ isActive }) => isActive ? 'nav-link active' : 'nav-link'}>
+//                 Logs
+//             </NavLink>
+//         </div>
+//     );
+// };
+
+// export default Navbar;
